31c38f852b828a57b0dfca9b5684985a9f23d463
James Pants <jamespants@gmail.com>
2014-12-15 12:16:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var AWS, CronJob, app, express, http, path, port, server, Tabletop;
+
+AWS = require('aws-sdk');
+CronJob = require('cron').CronJob;
+Tabletop = require('tabletop');
+
+AWS.config.loadFromPath('./aws.json');
+
+
+
+function writeJsonToAmazon(array){
+	var s3 = new AWS.S3();
+
+	var params = {
+		Bucket: 'yellow-flyers-data',
+		ACL: 'public-read',
+		Key: 'flyers.json',
+		ContentType: 'application/javascript',
+		Body: JSON.stringify(array)
+	};
+
+	s3.putObject(params, function(err, data){
+		if (err){
+			console.log(err);
+		}
+		else{
+			console.log('flyers.json updated');
+		}
+	});
+}
+
+//updates data
+
+function updateData(){
+
+	Tabletop.init({
+		key: '1v-aQYwpoUJEcopld4DsYafoOuDXIgJfPepuWwQcd2RY',
+		callback: function(data){
+			console.log(data);
+			writeJsonToAmazon(data);
+		}
+	});
+
+}
+
+updateData();
+
+//scheduled tasks
+
+new CronJob('00-60/30 * * * * *', function() {
+
+  // ...and every minute afterwards
+
+  updateData();
+
+}, null, true);
